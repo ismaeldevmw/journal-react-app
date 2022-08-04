3649c82a87d70f6d3d6df9c02a671742bd58258e
Ismael López <ismaellopez.dev@gmail.comm>
2022-08-03 22:15:04 -0500

    :lipstick: [ADD] Register page design

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <AuthLayout title="Login">
+    <AuthLayout title="Iniciar sesión">
         <form>
           <Grid container>
             <Grid item xs={12} sx={{ mt: 2 }}>
@@ -29,7 +29,7 @@ export const LoginPage = () => {
             <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
               <Grid item xs={12} sm={6}>
                 <Button variant="contained" fullWidth>
-                  Login
+                  Entrar
                 </Button>
               </Grid>
               <Grid item xs={12} sm={6}>
