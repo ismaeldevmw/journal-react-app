1d0230aded252ba78648e69b133f23b6fa8eaee8
Ismael López <ismaellopez.dev@gmail.comm>
2022-11-22 09:19:01 -0600

    :lipstick: :add Journal NavBar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,15 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h2" component="h1">JournalPage</Typography>
-      <MailOutline/>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae commodi amet voluptas tenetur at laboriosam dolorum, nulla quaerat dicta maiores expedita omnis, nostrum doloribus, blanditiis rem placeat adipisci. Accusantium, voluptatibus!</Typography>
+
+      {/* NothingSelected */}
+      {/* NoteView */}
+      
+    </JournalLayout>
   )
 }
